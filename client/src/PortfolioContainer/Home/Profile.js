4b9ce9b7a7fa561d284df0cd8e370ad6f5e9328e
cesar-infante <cesar261110@gmail.com>
2022-09-28 22:50:34 -0700

    testing to Profile.js

--- client/src/PortfolioContainer/Home/Profile.js
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Typewriter } from 'react-simple-typewriter'
 
 export default function Profile() {
     return (
@@ -15,6 +16,41 @@ export default function Profile() {
                         <a href="https://www.linkedin.com/in/cesarinfante/" target="_blank" rel="noreferrer">
                             <i class="fa-brands fa-linkedin icon"></i>
                         </a>
+                    </div>
+
+                    <div className='profile-details-name'>
+                        <span className='primary-text'>
+                            Hello, I'm <span className='highlighted-text'>Cesar</span>
+                        </span>
+                    </div>
+                    <div className='profile-details-role'>
+                        <span className='primary-text'>
+                            <h1>
+                                <Typewriter
+                                    words={[
+                                        "Software Engineer 👨🏽‍💻",
+                                        "Ingeniero de Software 🇲🇽",
+                                        "MERN Stack Dev 💻",
+                                        "From warehouse Team Lead to Software Engineer! 🧑🏽‍💼"
+                                    ]}
+                                    loop={true}
+                                    typeSpeed={70}
+                                    deleteSpeed={45}
+                                    cursor
+                                />
+                            </h1>
+                            <span className='profile-role-tagline'>
+                                Coming from a background in warehouse leadership, teaching me discipline,
+                                achieving deadlines, and constant growth. My main approach is the 5 P's,
+                                "Proper Preparation Prevents Poor Performance". This approach ensures
+                                I put in the time to learn and be prepared and not surprised.
+                            </span>
+                        </span>
+                    </div>
+                    <div className='profile-options'>
+                        <button className='btn primary-btn'>
+                            Hire Me 
+                        </button>
                     </div>
                 </div>
             </div>
