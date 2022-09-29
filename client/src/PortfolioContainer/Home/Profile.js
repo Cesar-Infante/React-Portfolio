bafa4256e444311c6900f1ed0229cb1ab8c0f174
cesar-infante <cesar261110@gmail.com>
2022-09-28 21:05:30 -0700

    Initial files along with testing to client folder

---
+++ client/src/PortfolioContainer/Home/Profile.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+export default function Profile() {
+    return (
+        <div className='profile-container'>
+            <div className='profile-parent'>
+                <div className='profile-details'>
+                    <div className='icons'>
+                        <a href="mailto: cesar261110@gmail.com" target="_blank" rel="noreferrer">
+                            <i class="fa-solid fa-envelope icon"></i>
+                        </a>
+                        <a href="https://github.com/Cesar-Infante" target="_blank" rel="noreferrer">
+                            <i class="fa-brands fa-github icon"></i>
+                        </a>
+                        <a href="https://www.linkedin.com/in/cesarinfante/" target="_blank" rel="noreferrer">
+                            <i class="fa-brands fa-linkedin icon"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
